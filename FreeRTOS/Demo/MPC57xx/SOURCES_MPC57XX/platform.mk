a9e57ccf59c6588d5a09a2696bc74d578cfe5a89
Frey, Jedediah <JedediahFrey@eaton.com>
2017-05-28 20:08:05 -0400

    Development Sprint Auto Commit: Sun, May 28, 2017  8:08:05 PM

--- FreeRTOS/Demo/MPC57xx/SOURCES_MPC57XX/platform.mk
+++ FreeRTOS/Demo/MPC57xx/SOURCES_MPC57XX/platform.mk
@@ -1,7 +1,7 @@
 #==================================================================================================
 #
 #  (c) Copyright 2015 Freescale Semiconductor Inc.
-#
+#  
 #  This program is free software; you can redistribute it and/or modify it under
 #  the terms of the GNU General Public License (version 2) as published by the
 #  Free Software Foundation >>>> AND MODIFIED BY <<<< the FreeRTOS exception.
@@ -17,17 +17,17 @@
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-#
+#  
 #  You should have received a copy of the GNU General Public License
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #
 #================================================================================================*/
 OBJFILES_PLATFORM  :=  $(OUT_ROOT)/obj/$(TEST_NAME)/hw_flashrchw.o \
-                       $(OUT_ROOT)/obj/$(TEST_NAME)/hw_platform.o
+                       $(OUT_ROOT)/obj/$(TEST_NAME)/hw_platform.o 
 
 $(OUT_ROOT)/obj/$(TEST_NAME)/%.o: ../SOURCES_MPC57XX/%.c
-	$(CC) $(CFLAGS) -c -o $@ $<
-
+	$(CC) $(CFLAGS) -c -o $(shell $(CYGPATH) -m -i $@) $<					   
+					   
 $(OUT_ROOT)/obj/$(TEST_NAME)/%.o: ../SOURCES_MPC57XX/%.s
-	$(AS) $(ASFLAGS) -o $@ $<
+	$(AS) $(ASFLAGS) -o $(shell $(CYGPATH) -m -i $@) $<
